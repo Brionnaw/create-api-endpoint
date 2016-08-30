b68368435e5994d9cd98c90f54af58edd60ba1c0
Brionna White <Brionnanwhite@gmail.com>
2016-08-30 14:08:20 -0700

    create post

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 "use strict";
 var express = require('express');
 var app = express();
-app.get('/:id', (function (req, res, next) {
-    if (req.params) {
-        console.log('success');
-    }
-    else {
-        next();
-    }
-}, function (req, res) {
-    res.send('error, please include an id');
-}));
+app.post('/movies/', function (req, res) {
+    console.log(req.body);
+    res.send(req.body);
+});
 var server = app.listen(3000, function () {
     var port = server.address().port;
     console.log("Server is running on PORT:" + port);
--- server.ts
+++ server.ts
@@ -1,16 +1,11 @@
 import express = require('express');
 let app = express();
-// route handler: handling functions within your route,  //
-  app.get('/:id', ((req, res, next) =>{ //next tells the function to go to the part of the process //
-    if(req.params){
-    console.log('success');
-  } else {
-    next();
-    }
-  }, function(req, res) {
 
-    res.send('error, please include an id');
-  }));
+  app.post('/movies/', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+
+  })
 
 let server = app.listen(3000, function () {
   let port = server.address().port;
