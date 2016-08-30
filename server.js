08e64ed6703a04d5cc846de910607012fa2614bf
Brionna White <Brionnanwhite@gmail.com>
2016-08-30 14:05:46 -0700

    chaining routes

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 "use strict";
 var express = require('express');
 var app = express();
-app.get('/about', (function (req, res) {
-    res.send(' I am a GET request');
-}));
-app.post('/new', (function (req, res) {
-    res.send(' I am a post request');
-}));
+app.route('/movies')
+    .get(function (req, res) {
+    res.send('GET a movie');
+})
+    .post(function (req, res) {
+    res.send('POST a movie');
+})
+    .put(function (req, res) {
+    res.send('PUT a movie');
+})
+    .delete(function (req, res) {
+    res.send('DELETE a movie');
+});
 var server = app.listen(3000, function () {
     var port = server.address().port;
     console.log("Server is running on PORT:" + port);
--- server.ts
+++ server.ts
@@ -1,13 +1,20 @@
 import express = require('express');
 let app = express();
+// chaining routes - condensing the route path into one line of code, changing the route methodss.//
 
-  app.get('/about',  ((req, res) => {
-    res.send(' I am a GET request');
-  })) // api endpoint
-
-app.post('/new', ((req, res) => {
-  res.send (' I am a post request');
-}))
+  app.route('/movies')
+    .get((req, res) => {
+    res.send('GET a movie');
+  })
+    .post((req, res) => {
+    res.send('POST a movie');
+  })
+    .put((req, res) => {
+    res.send('PUT a movie');
+  })
+    .delete((req, res) => {
+    res.send('DELETE a movie');
+  });
 
 let server = app.listen(3000, function () {
   let port = server.address().port;
